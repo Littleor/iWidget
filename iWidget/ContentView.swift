850cfa84ff7ce3886b2c2f79bf844d375e50c1c1
Little <admin@sixming.com>
2020-06-25 10:54:03 +0800

    Initial Commit

---
+++ iWidget/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iWidget
+//
+//  Created by Littleor on 2020/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iWidget/iWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWidgetApp.swift
+//  iWidget
+//
+//  Created by Littleor on 2020/6/25.
+//
+
+import SwiftUI
+
+@main
+struct iWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
