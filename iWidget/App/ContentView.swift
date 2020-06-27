0d8a5ba902aee57397d9b7e5f4cedf35e844cd24
Little <admin@sixming.com>
2020-06-28 01:52:57 +0800

    去除注释修复支付小部件不跳转bug

--- iWidget/App/ContentView.swift
+++ iWidget/App/ContentView.swift
@@ -11,60 +11,61 @@ struct ContentView: View {
     @State var showWidget = false
     @State var viewState = CGSize.zero
     var body: some View {
-        //        NavigationView {
         
-        VStack {
-            HStack {
-                Text("iWidget")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.all)
-            }
-            .blur(radius: showWidget ? 20 : 0)
-            Spacer()
-            ZStack{
-                WidgetCardView(kind: "oneWord")
-                    .offset(x: 0, y: showWidget ? -200.0 : -20.0)
-                    .scaleEffect(0.95)
-                    .rotationEffect(Angle(degrees: showWidget ? 10 : 0))
-                    .animation(.easeIn)
-                WidgetCardView()
-                    .rotationEffect(Angle(degrees: showWidget ? 5 : 0))
-                    .animation(.spring())
-                    .onTapGesture {
-                        self.showWidget.toggle()
-                    }
-            }
-            Spacer()
-            VStack{
-                Text("一款简单开源的小部件工具箱")
-                    .font(.footnote)
-                    .onTapGesture {
-                        UIApplication.shared.open(URL(string: "https://github.com/Littleor/iWidget")!)
-                    }
-                HStack{
-                    Text("Coded By")
-                        .padding(.all, 0)
-                        .font(.footnote)
-                    Text("Littleor")
-                        .padding(.all, 0)
-                        .font(.footnote)
-                        .foregroundColor(.blue)
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("iWidget")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.all)
+                }
+                .blur(radius: showWidget ? 20 : 0)
+                Spacer()
+                ZStack{
+                    WidgetCardView(kind: "oneWord")
+                        .offset(x: 0, y: showWidget ? -200.0 : -20.0)
+                        .scaleEffect(0.95)
+                        .rotationEffect(Angle(degrees: showWidget ? 10 : 0))
+                        .animation(.easeIn)
+                    WidgetCardView()
+                        .rotationEffect(Angle(degrees: showWidget ? 5 : 0))
+                        .animation(.spring())
                         .onTapGesture {
-                            UIApplication.shared.open(URL(string: "https://github.com/Littleor")!)
+                            self.showWidget.toggle()
                         }
                 }
-            }.blur(radius: showWidget ? 20 : 0)
+                Spacer()
+                VStack{
+                    Text("一款简单开源的小部件工具箱")
+                        .font(.footnote)
+                        .onTapGesture {
+                            UIApplication.shared.open(URL(string: "https://github.com/Littleor/iWidget")!)
+                        }
+                    HStack{
+                        Text("Coded By")
+                            .padding(.all, 0)
+                            .font(.footnote)
+                        Text("Littleor")
+                            .padding(.all, 0)
+                            .font(.footnote)
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                UIApplication.shared.open(URL(string: "https://github.com/Littleor")!)
+                            }
+                    }
+                }.blur(radius: showWidget ? 20 : 0)
+                
+            }
             
         }
-        
-        //        }
-        //        .onOpenURL(perform: { (url) in
-        //            UIApplication.shared.open(url)
-        //        })
+        .onOpenURL(perform: { (url) in
+            UIApplication.shared.open(url)
+        })
         
     }
 }
+
 
 struct WidgetCardView: View {
     var kind = "payTools"
