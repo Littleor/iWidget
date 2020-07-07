09aa433a4e215fdb38936184acd5d5bb0b1262f0
Little <admin@sixming.com>
2020-07-07 11:50:26 +0800

    完善首页

--- iWidget/App/ContentView.swift
+++ iWidget/App/ContentView.swift
@@ -23,13 +23,26 @@ struct ContentView: View {
                 .blur(radius: showWidget ? 20 : 0)
                 Spacer()
                 ZStack{
-                    WidgetCardView(kind: "oneWord")
-                        .offset(x: 0, y: showWidget ? -200.0 : -20.0)
-                        .scaleEffect(0.95)
-                        .rotationEffect(Angle(degrees: showWidget ? 10 : 0))
+                    WidgetCardView(kind: "RSSReader")
+                        .offset(x: 0, y: showWidget ? -450.0 : -60.0)
+                        .scaleEffect(0.85)
+                        .rotationEffect(Angle(degrees: showWidget ? 4 : 0))
                         .animation(.easeIn)
+                    
+                    WidgetCardView(kind: "Idiom")
+                        .offset(x: 0, y: showWidget ? -300.0 : -40.0)
+                        .scaleEffect(0.90)
+                        .rotationEffect(Angle(degrees: showWidget ? 3 : 0))
+                        .animation(.easeIn)
+                    
+                    WidgetCardView(kind: "oneWord")
+                        .offset(x: 0, y: showWidget ? -150.0 : -20.0)
+                        .scaleEffect(0.95)
+                        .rotationEffect(Angle(degrees: showWidget ? 2 : 0))
+                        .animation(.easeIn)
+                    
                     WidgetCardView()
-                        .rotationEffect(Angle(degrees: showWidget ? 5 : 0))
+                        .rotationEffect(Angle(degrees: showWidget ? 1 : 0))
                         .animation(.spring())
                         .onTapGesture {
                             self.showWidget.toggle()
@@ -76,6 +89,9 @@ struct WidgetCardView: View {
                 switch kind {
                 case "payTools":PayToolsMediumView()
                 case "oneWord":OneWordView(content: "一言部件,放置后每小时自动刷新")
+                case "Idiom": IdiomView(idiom: Idiom(word: "诚惶诚恐", pinyin: "chéng huáng chéng kǒng", explanation: "诚实在，的确；惶害怕；恐畏惧。非常小心谨慎以至达到害怕不安的程度。", derivation: "汉·杜诗《乞退郡疏》奉职无效，久窃禄位，令功臣怀愠，诚惶诚恐。", example: "一些成了惊弓之鸟的部员们算也～地先后把那段危险的地面通过了。★郭沫若《北伐途中》二十一"))
+                case "RSSReader":
+                    RSSReaderView(title: "iWidget", itemTitle: "iOS14首款小部件集合软件-iWidget", itemDesc: "iOS14首款小部件集合软件-iWidget", itemPubDate: "刚刚", url: "https://github.com/Littleor/iWidget/")
                 default:PayToolsMediumView()
                 }
                 Spacer()
