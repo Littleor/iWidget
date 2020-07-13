127dceda3586d9183d8f4c46575ec007aa25590f
Little <admin@sixming.com>
2020-07-13 21:58:48 +0800

    单词小部件PlaceHolder修正

--- MainWidget/Widget/EnglishWord.swift
+++ MainWidget/Widget/EnglishWord.swift
@@ -68,9 +68,9 @@ struct EnglishWordWidget: Widget {
         .supportedFamilies([.systemMedium]) 
     }
 }
-struct MainWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        EnglishWordEntryView(entry:EnglishWordEntry(date: Date(),data: [EnglishWord(word: "abandon", tran: ["v. 遗弃;离开;放弃;终止;陷入","n. 放任,狂热"], ch_example: "他声称他的父母抛弃了他。", en_example: "He claimed that his parents had abandoned him.", exam: ["TOEFL","高中","SAT","IELTS","CET6","GRE","CET4","商务英语","考研"], uspronunciation: "əˈbændən", ukpronunciation: "əˈbændən", type: "CET4"),EnglishWord(word: "abandon", tran: ["v. 遗弃;离开;放弃;终止;陷入","n. 放任,狂热"], ch_example: "他声称他的父母抛弃了他。", en_example: "He claimed that his parents had abandoned him.", exam: ["TOEFL","高中","SAT","IELTS","CET6","GRE","CET4","商务英语","考研"], uspronunciation: "əˈbændən", ukpronunciation: "əˈbændən", type: "CET4"),EnglishWord(word: "abandon", tran: ["v. 遗弃;离开;放弃;终止;陷入","n. 放任,狂热"], ch_example: "他声称他的父母抛弃了他。", en_example: "He claimed that his parents had abandoned him.", exam: ["TOEFL","高中","SAT","IELTS","CET6","GRE","CET4","商务英语","考研"], uspronunciation: "əˈbændən", ukpronunciation: "əˈbændən", type: "CET4")]))
-            .previewContext(WidgetPreviewContext(family: .systemMedium))
-    }
-}
+//struct MainWidget_Previews: PreviewProvider {
+//    static var previews: some View {
+//        EnglishWordEntryView(entry:EnglishWordEntry(date: Date(),data: [EnglishWord(word: "abandon", tran: ["v. 遗弃;离开;放弃;终止;陷入","n. 放任,狂热"], ch_example: "他声称他的父母抛弃了他。", en_example: "He claimed that his parents had abandoned him.", exam: ["TOEFL","高中","SAT","IELTS","CET6","GRE","CET4","商务英语","考研"], uspronunciation: "əˈbændən", ukpronunciation: "əˈbændən", type: "CET4"),EnglishWord(word: "abandon", tran: ["v. 遗弃;离开;放弃;终止;陷入","n. 放任,狂热"], ch_example: "他声称他的父母抛弃了他。", en_example: "He claimed that his parents had abandoned him.", exam: ["TOEFL","高中","SAT","IELTS","CET6","GRE","CET4","商务英语","考研"], uspronunciation: "əˈbændən", ukpronunciation: "əˈbændən", type: "CET4"),EnglishWord(word: "abandon", tran: ["v. 遗弃;离开;放弃;终止;陷入","n. 放任,狂热"], ch_example: "他声称他的父母抛弃了他。", en_example: "He claimed that his parents had abandoned him.", exam: ["TOEFL","高中","SAT","IELTS","CET6","GRE","CET4","商务英语","考研"], uspronunciation: "əˈbændən", ukpronunciation: "əˈbændən", type: "CET4")]))
+//            .previewContext(WidgetPreviewContext(family: .systemMedium))
+//    }
+//}
